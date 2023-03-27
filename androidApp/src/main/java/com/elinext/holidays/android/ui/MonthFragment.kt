8b83e09015283f1388de1e95c3660cfc0450a3f7
A.Cherenkevich <4erenkevi4@gmail.com>
2023-03-27 18:00:42 +0300

    added click for month item in Year screen part 2

--- androidApp/src/main/java/com/elinext/holidays/android/ui/MonthFragment.kt
+++ androidApp/src/main/java/com/elinext/holidays/android/ui/MonthFragment.kt
@@ -2,25 +2,76 @@ package com.elinext.holidays.android.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import com.elinext.holidays.android.MyApplicationTheme
 import com.kizitonwose.calendar.compose.CalendarState
 import com.kizitonwose.calendar.compose.HorizontalCalendar
 import com.kizitonwose.calendar.compose.rememberCalendarState
 import com.kizitonwose.calendar.core.daysOfWeek
-import java.io.Serializable
 import java.time.DayOfWeek
 import java.time.YearMonth
 
 class MonthFragment : BaseFragment() {
 
-    var restoreYear: Int? = null
-    var restoreMonth: Int? = null
+    var restoredYear: Int? = null
+        get() = if (field == 99) null else field
+        private set
+    var restoredMonth: Int? = null
+        get() = if (field == 99) null else field
+        private set
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        restoreYear = arguments?.getInt("year", 99)
-        restoreMonth = arguments?.getInt("month", 99)
+        restoredYear = arguments?.getInt("year", 99)
+        restoredMonth = arguments?.getInt("month", 99)
         super.onViewCreated(view, savedInstanceState)
 
+    }
+
+    @Composable
+    override fun GreetingView() {
+        val yearMonth = if (restoredYear != null && restoredMonth != null) YearMonth.of(
+            restoredYear!!,
+            restoredMonth!!
+        ) else YearMonth.now()
+        MyApplicationTheme {
+            val currentMonth = remember { yearMonth}
+            val startMonth = remember { currentMonth.minusMonths(100) } // Adjust as needed
+            val endMonth = remember { currentMonth.plusMonths(100) } // Adjust as needed
+            val calendarState = rememberCalendarState(
+                startMonth = startMonth,
+                endMonth = endMonth,
+                firstVisibleMonth = currentMonth,
+                firstDayOfWeek = DayOfWeek.MONDAY
+            )
+            Scaffold(
+                modifier = Modifier
+                    .fillMaxSize(),
+                topBar = {
+                    TopBar(getTitle(calendarState))
+                },
+                backgroundColor = MaterialTheme.colors.background,
+                contentColor = Color.White
+            ) { value ->
+                val padding = value
+                Surface(
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    // CustomTabs(calendarState)
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        CustomTabs(calendarState)
+                    }
+                }
+            }
+        }
     }
 
     @Composable
@@ -39,15 +90,12 @@ class MonthFragment : BaseFragment() {
 
     @Composable
     override fun CalendarContent(calendarState: CalendarState) {
-        val state = if (restoreYear == 99 && restoreMonth == 99) calendarState else state(
-            year = restoreYear!!,
-            month = restoreMonth!!
-        )
+
         DaysOfWeekTitle(daysOfWeek(firstDayOfWeek = DayOfWeek.MONDAY))
         HorizontalCalendar(
-            state = state,
+            state = calendarState,
             dayContent = { Day(it) }
         )
-        HolidaysView(state)
+        HolidaysView(calendarState)
     }
 }
