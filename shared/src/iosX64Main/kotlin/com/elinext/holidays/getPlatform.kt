f9fb0c94cc2b17d7d1fee9acbba500ea90e0b455
A.Cherenkevich <4erenkevi4@gmail.com>
2023-08-07 12:35:23 +0200

    IOS Buildability

--- shared/src/iosX64Main/kotlin/com/elinext/holidays/getPlatform.kt
+++
@@ -1,8 +0,0 @@
-package com.elinext.holidays
-
-import platform.UIKit.UIDevice
-
-class IOSPlatform: Platform {
-    override val name: String = UIDevice.currentDevice.systemName() + " " + UIDevice.currentDevice.systemVersion
-}
-
